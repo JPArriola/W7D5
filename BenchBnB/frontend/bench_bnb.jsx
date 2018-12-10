bc6f28554d921df4f4bfe930966b25a3cdaff395
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-10 12:23:47 -0800

    Add Greeting container

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, logout, signup} from './util/session_api_util';
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
-
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root)
+  ReactDOM.render(<Root store={store}/>, root)
 });
---
+++ BenchBnB/frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import greeting_container from './greeting/greeting_container';
+
+const App = () => (
+  <div>
+    <h1>Josh BnB</h1>
+  </div>
+);
+
+export default App;
